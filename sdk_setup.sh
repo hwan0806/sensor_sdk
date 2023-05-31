e7150f70e677d6374b8b2a768e9a33a0c405a2cc
gil <rlfghks527@naver.com>
2023-06-01 00:09:12 +0900

    udpdate setup.sh

--- sdk_setup.sh
+++ sdk_setup.sh
@@ -1,11 +1,11 @@
 # sensor SDK setup for SparoLab
 
-# 1. Livox-SDK (Livox LiDAR - AVIA )
+# 1. Livox-SDK for Livox-AVIA (https://github.com/Livox-SDK/Livox-SDK#4-livox-sdk-api )
 cd Livox-SDK/build
 cmake .. && make
 sudo make install
 
-# 2. Livox-SDK2 (Livox LiDAR - MID360 )
+# 2. Livox-SDK2 for Livox-MID360 (https://github.com/Livox-SDK/Livox-SDK2 )
 cd ../../Livox-SDK2 && mkdir build
 cd build
 cmake .. && make -j
